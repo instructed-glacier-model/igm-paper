c2fb796b9454cbc2c6da28a9756605fc12677587
Guillaume Jouvet <guillaume.jouvet@gmail.com>
2024-04-28 18:32:46 +0200

    add enthalpy expriments param file to the folder

--- clean.sh
+++ clean.sh
@@ -1,1 +1,1 @@
-rm paper.aux paper.bbl paper.blg paper.log paper.synctex.gz
+rm paper.aux paper.bbl paper.blg paper.log paper.synctex.gz paper.fdb_latexmk paper.fls
---
+++ experiments/test-Kleiner/sol_analytic/README.md
@@ -0,0 +1,1 @@
+Data taken from Zhang Tong and Wang Yuzhe github repository : https://github.com/WangYuzhe/PoLIM-Polythermal-Land-Ice-Model
